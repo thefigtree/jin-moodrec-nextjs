a0771e0d0e225c515795cd9ce097a3cf241d3741
thefigtree <ldj8431@naver.com>
2024-10-15 00:35:05 +0900

    Feat: Add DashboardSection UI Publishing

--- src/components/dashboard-section.tsx
+++ src/components/dashboard-section.tsx
@@ -1,3 +1,69 @@
+import { Fugaz_One } from "next/font/google";
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
+
 export default function DashboardSection() {
-  return <div>dashboard-section</div>;
+  const statues = {
+    num_days: 14,
+    time_remaining: "13:14:26",
+    date: new Date().toDateString(),
+  };
+
+  const moods = {
+    "$%^&*": "🤪",
+    Sad: "🥲",
+    Existing: "😶",
+    Good: "😆",
+    Elated: "😍",
+  };
+
+  return (
+    <div className="flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16">
+      <div className="grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500">
+        {Object.keys(statues).map((status, index) => {
+          return (
+            <div key={index} className="p-4 flex flex-col gap-1 sm:gap-2">
+              <p className="font-medium uppercase text-xs sm:text-sm">
+                {status.replaceAll("_", "")}
+              </p>
+              <p className={"text-base sm:text-lg " + fugaz.className}>
+                {statues[status]}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+
+      <h4
+        className={
+          "text-4xl sm:text-5xl md:text-6xl text-center " + fugaz.className
+        }
+      >
+        How do you <span className="textGradient">feel</span> today ?
+      </h4>
+
+      <div className="flex items-stretch flex-wrap gap-4">
+        {Object.keys(moods).map((mood, moodIndex) => {
+          return (
+            <button
+              className={
+                "p-4 px-5 rounded-2xl pupleShadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center flex flex-col gap-2 items-center flex-1 "
+              }
+              key={moodIndex}
+            >
+              <p className="text-4xl sm:text-5xl md:text-6xl">{moods[mood]}</p>
+              <p
+                className={
+                  "text-indigo-500 text-xs sm:text-sm md:text-base " +
+                  fugaz.className
+                }
+              >
+                {mood}
+              </p>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
